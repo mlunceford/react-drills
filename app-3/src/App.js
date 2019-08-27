a87057777adf00acad4e620cc8e7b20bd4e5d47e
mlunceford <mark7lunceford@gmail.com>
2019-08-27 16:20:32 -0600

    app3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      message: '',
+      teams: ['jazz', 'lakers', 'celtics', 'hornets', 'nuggeets']
+    }
+  }
+  handleChange(value){
+    this.setState({message: value});
+  }
   render() {
+    const teamNames = this.state.teams
+    .filter((e,i) => {
+      return e.includes(this.state.message);
+    })
+    .map((e,i)=>{
+      return <h2 key={i}> {e}</h2>
+    })  
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={e => this.handleChange(e.target.value)} type='text'/>
+        <h1>{teamNames}</h1>
       </div>
     );
   }
